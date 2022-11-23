c08efe8816b1d877f07d092eb7b477aa429c92cb
Dominik Schramm <Dominik.Schramm@activesolution.at>
2022-11-23 13:55:04 +0100

    struct and tuple struct

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 use std::io;
 
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+struct Area(u32, String);
+
 fn main() {
-    println!("Hello... Who are you?");
+    println!("Rectangle Area Calculator");
+    println!("Please enter the width of the rectangle:");
 
-    let mut name = String::new();
-    io::stdin()
-        .read_line(&mut name)
-        .expect("Failed to read line");
-    name = name.trim_end().to_string();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let width: u32 = input.trim().parse().expect("Please type a number!");
+    input.clear();
 
-    println!("Hello, {}!", name);
+    println!("Please enter the height of the rectangle:");
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let height: u32 = input.trim().parse().expect("Please type a number!");
+
+    let rect = Rectangle { width, height };
+    let area = Area(rect.width * rect.height, "qm".to_string());
+
+    println!("The area of the rectangle is {}{}", area.0, area.1);
 }
